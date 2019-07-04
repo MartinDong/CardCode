43ec0f64bc7175623de31612ae2c797207825da4
donghongyu <1358506549@qq.com>
2019-07-04 14:08:16 +0800

    完成了 图片灰度化 二值化 图像腐蚀

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class Main {
     static {
@@ -13,15 +16,41 @@ public class Main {
         System.out.println("Hello word!");
         String originalImgPath = "D:\\lunwen\\CardCode\\src\\main\\resources\\test_img\\test1.jpg";
 
-        Mat mat = Imgcodecs.imread(originalImgPath);
-        System.out.println(mat);
-        if (mat.dataAddr() == 0) {
+        Mat destMat = new Mat();
+
+        // 1. 读取图片
+        Mat srcMat = Imgcodecs.imread(originalImgPath);
+        System.out.println(srcMat);
+        if (srcMat.dataAddr() == 0) {
             System.out.println("打开文件出错");
         }
 
-        ImageViewer imageViewer = new ImageViewer(mat, "第一幅图片");
+        // 2.图片灰度化,就是把图片转换为黑白照
+        Imgproc.cvtColor(srcMat, destMat, Imgproc.COLOR_RGB2GRAY);
+
+        // 3.二值化处理,也就是只留两个值,黑白
+        Mat binaryMat = new Mat(srcMat.height(), srcMat.width(), CvType.CV_8UC1);
+        Imgproc.threshold(destMat, binaryMat, 100, 255, Imgproc.THRESH_BINARY);
+
+        // 4.图像腐蚀,这里使用3*3的图片去腐蚀
+        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+        Imgproc.erode(binaryMat, destMat, element);
+
+//        // 5.遍历,获取和修改像素值,二值图片,当成一个二维矩阵就可以了,双重循环,使用get方法获取像素点,使用put方法修改像素点
+//        for (int y = 0; y < destMat.height(); y++) {
+//            for (int x = 0; x < destMat.width(); x++) {
+//                //得到该行像素点的值
+//                double[] data = destMat.get(y, x);
+//                for (int i1 = 0; i1 < data.length; i1++) {
+//                    data[i1] = 0;//像素点都改为白色
+//                }
+//                destMat.put(y, x, data);
+//            }
+//        }
+
+        // 展示处理结果
+        ImageViewer imageViewer = new ImageViewer(destMat, "图片预览");
         imageViewer.imshow();
-//        ShowImage window = new ShowImage(mat);
-//        window.getFrame().setVisible(true);
+
     }
 }
