044dcb09ecd17af994a999733258e679e1c5d1ca
donghongyu <1358506549@qq.com>
2019-07-04 10:41:31 +0800

    完成了基础配置

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class Main {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        //注意程序运行的时候需要在VM option添加该行 指明opencv的dll文件所在路径
+        //-Djava.library.path=$PROJECT_DIR$\opencv\x64
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello word!");
+
+        Mat mat = Imgcodecs.imread("D:\\lunwen\\CardCode\\src\\main\\test_img\\test1.jpg");
+        System.out.println(mat);
     }
 }
