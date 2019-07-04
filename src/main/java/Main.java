dea6c8dcafb7d3e515b01105c0b0608d1887f8d7
MartinDong <1358506549@qq.com>
2019-07-05 01:18:57 +0800

    完成了车牌的截取工作，部分白色车辆识别效果较差

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static {
@@ -14,43 +14,113 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello word!");
-        String originalImgPath = "D:\\lunwen\\CardCode\\src\\main\\resources\\test_img\\test1.jpg";
+        String originalImgPath = "D:\\AOpnecv\\CardCode\\src\\main\\resources\\test_img\\test8.jpg";
 
-        Mat destMat = new Mat();
-
-        // 1. 读取图片
+        // 1、 读取图片
         Mat srcMat = Imgcodecs.imread(originalImgPath);
         System.out.println(srcMat);
         if (srcMat.dataAddr() == 0) {
             System.out.println("打开文件出错");
         }
 
-        // 2.图片灰度化,就是把图片转换为黑白照
-        Imgproc.cvtColor(srcMat, destMat, Imgproc.COLOR_RGB2GRAY);
+        // 2、图片灰度化,就是把图片转换为黑白照
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(srcMat, grayImage, Imgproc.COLOR_RGB2GRAY);
 
-        // 3.二值化处理,也就是只留两个值,黑白
-        Mat binaryMat = new Mat(srcMat.height(), srcMat.width(), CvType.CV_8UC1);
-        Imgproc.threshold(destMat, binaryMat, 100, 255, Imgproc.THRESH_BINARY);
+        // 3、使用Canndy检测边缘
+        Mat cannyImage = new Mat();
+        double lowThresh = 500;//双阀值抑制中的低阀值
+        double heightThresh = 200;//双阀值抑制中的高阀值
+        Imgproc.Canny(grayImage, cannyImage, lowThresh, heightThresh,3);
 
-        // 4.图像腐蚀,这里使用3*3的图片去腐蚀
-        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
-        Imgproc.erode(binaryMat, destMat, element);
+        // 4、形态学（膨胀腐蚀）处理
+        // 图片膨胀处理
+        // 膨胀
+        Mat dilateImage = new Mat();
+        // 侵蚀
+        Mat erodeImage = new Mat();
+        Mat elementX = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(25, 1));
+        Mat elementY = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, 19));
+        Point point = new Point(-1, -1);
 
-//        // 5.遍历,获取和修改像素值,二值图片,当成一个二维矩阵就可以了,双重循环,使用get方法获取像素点,使用put方法修改像素点
-//        for (int y = 0; y < destMat.height(); y++) {
-//            for (int x = 0; x < destMat.width(); x++) {
-//                //得到该行像素点的值
-//                double[] data = destMat.get(y, x);
-//                for (int i1 = 0; i1 < data.length; i1++) {
-//                    data[i1] = 0;//像素点都改为白色
-//                }
-//                destMat.put(y, x, data);
-//            }
-//        }
+        // 自定义 核进行 x 方向的膨胀腐蚀
+        Imgproc.dilate(cannyImage, dilateImage, elementX, point, 2);
+        Imgproc.erode(dilateImage, erodeImage, elementX, point, 4);
+        Imgproc.dilate(erodeImage, dilateImage, elementX, point, 2);
+
+        // 自定义 核进行 Y 方向的膨胀腐蚀
+        Imgproc.erode(dilateImage, erodeImage, elementY, point, 1);
+        Imgproc.dilate(erodeImage, dilateImage, elementY, point, 2);
+
+        // 噪声处理
+        // 平滑处理 中值滤波
+        Mat blurrImage = new Mat();
+        Imgproc.medianBlur(dilateImage, blurrImage, 15);
+        Imgproc.medianBlur(blurrImage, blurrImage, 15);
 
         // 展示处理结果
-        ImageViewer imageViewer = new ImageViewer(destMat, "图片预览");
+        ImageViewer blurrImageViewer = new ImageViewer(blurrImage, "图片预览");
+        blurrImageViewer.imshow();
+
+        // 5、轮廓处理
+        // 矩形轮廓查找与筛选：
+        Mat contourImage;
+        Mat hierarchyImage = new Mat();
+        // 深拷贝，查找轮廓会改变源图像信息，需要重新拷贝图像
+        contourImage = blurrImage.clone();
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(
+                contourImage,
+                contours,
+                hierarchyImage,
+                Imgproc.RETR_EXTERNAL,
+                Imgproc.CHAIN_APPROX_NONE);
+
+        System.out.println("轮廓数量：" + contours.size());
+        System.out.println("hierarchy类型：" + hierarchyImage);
+
+        // 画出轮廓
+        Imgproc.drawContours(contourImage, contours, -1, new Scalar(255), 1);
+        // 轮廓表示为一个矩形  车牌提取
+        Mat roiGrayImage = new Mat();
+        for (MatOfPoint contour : contours) {
+            Rect rectMin = Imgproc.boundingRect(contour);
+            System.out.println(
+                    "height = " + rectMin.height
+                            + "  width = " + rectMin.width +
+                            "rate = " + ((float) rectMin.width / rectMin.height)
+            );
+            if ((float) rectMin.width / rectMin.height >= 2.2 && (float) rectMin.width / rectMin.height <= 3.6) {
+                System.out.println("r.x = " + rectMin.x + "  r.y  = " + rectMin.y);
+                Imgproc.rectangle(srcMat, rectMin, new Scalar(0, 0, 255), 2);
+                roiGrayImage = srcMat.submat(rectMin);
+            }
+        }
+
+
+        // 6、自适应二值化处理
+        //Candy 边缘检测
+        Mat candyRoiImage = new Mat();
+        Imgproc.Canny(roiGrayImage, candyRoiImage, 450, 120, 3);
+        //二值化
+        Mat roiThreadHoldImage = new Mat();
+        Imgproc.threshold(candyRoiImage, roiThreadHoldImage, 50, 255, Imgproc.THRESH_BINARY);
+
+        // 展示处理结果
+        ImageViewer imageViewer = new ImageViewer(roiGrayImage, "图片预览");
         imageViewer.imshow();
 
+
+//        // 3.二值化处理,也就是只留两个值,黑白
+//        Mat binaryMat = new Mat(srcMat.height(), srcMat.width(), CvType.CV_8UC1);
+//        Imgproc.threshold(destMat, binaryMat, 50, 255, Imgproc.THRESH_BINARY);
+//
+//        // 4.图像腐蚀,这里使用3*3的图片去腐蚀
+//        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
+//        Imgproc.erode(binaryMat, destMat, element);
+//
+//        // 展示处理结果
+//        ImageViewer imageViewer = new ImageViewer(destMat, "图片预览");
+//        imageViewer.imshow();
     }
 }
